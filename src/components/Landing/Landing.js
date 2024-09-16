971ff0a9808126ad4bd6a12d8d19db6bb0f7646a
Ndiaga Ndiaye <njaga01@outlook.fr>
2024-09-16 18:29:14 +0000

    Fix bug Landing image

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,19 +1,14 @@
-import React, { useContext, useEffect } from 'react';
-import { Button } from '@material-ui/core';
-import { Link } from 'react-scroll';
-import { makeStyles } from '@material-ui/core/styles';
+import React, { useContext, useEffect } from "react";
+import { Button } from "@material-ui/core";
+import { Link } from "react-scroll";
+import { makeStyles } from "@material-ui/core/styles";
 
-import './Landing.css';
-import { ThemeContext } from '../../contexts/ThemeContext';
-import { headerData } from '../../data/headerData';
-import { socialsData } from '../../data/socialsData';
+import "./Landing.css";
+import { ThemeContext } from "../../contexts/ThemeContext";
+import { headerData } from "../../data/headerData";
+import { socialsData } from "../../data/socialsData";
 
-import {
-    FaGithub,
-    FaLinkedin,
-    FaTwitter,
-    FaFacebook,
-} from 'react-icons/fa';
+import { FaGithub, FaLinkedin, FaTwitter, FaFacebook } from "react-icons/fa";
 
 function Landing() {
     const { theme, drawerOpen } = useContext(ThemeContext);
@@ -21,48 +16,48 @@ function Landing() {
     const useStyles = makeStyles((t) => ({
         resumeBtn: {
             color: theme.primary,
-            borderRadius: '30px',
-            textTransform: 'inherit',
-            textDecoration: 'none',
-            width: '150px',
-            fontSize: '1rem',
-            fontWeight: '500',
-            height: '50px',
-            fontFamily: 'var(--primaryFont)',
+            borderRadius: "30px",
+            textTransform: "inherit",
+            textDecoration: "none",
+            width: "150px",
+            fontSize: "1rem",
+            fontWeight: "500",
+            height: "50px",
+            fontFamily: "var(--primaryFont)",
             border: `3px solid ${theme.primary}`,
-            transition: '100ms ease-out',
-            '&:hover': {
+            transition: "100ms ease-out",
+            "&:hover": {
                 backgroundColor: theme.tertiary,
                 color: theme.secondary,
                 border: `3px solid ${theme.tertiary}`,
             },
-            [t.breakpoints.down('sm')]: {
-                width: '180px',
+            [t.breakpoints.down("sm")]: {
+                width: "180px",
             },
         },
         contactBtn: {
             backgroundColor: theme.primary,
             color: theme.secondary,
-            borderRadius: '30px',
-            textTransform: 'inherit',
-            textDecoration: 'none',
-            width: '150px',
-            height: '50px',
-            fontSize: '1rem',
-            fontWeight: '500',
-            fontFamily: 'var(--primaryFont)',
+            borderRadius: "30px",
+            textTransform: "inherit",
+            textDecoration: "none",
+            width: "150px",
+            height: "50px",
+            fontSize: "1rem",
+            fontWeight: "500",
+            fontFamily: "var(--primaryFont)",
             border: `3px solid ${theme.primary}`,
-            transition: '100ms ease-out',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            '&:hover': {
+            transition: "100ms ease-out",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            "&:hover": {
                 backgroundColor: theme.secondary,
                 color: theme.tertiary,
                 border: `3px solid ${theme.tertiary}`,
             },
-            [t.breakpoints.down('sm')]: {
-                display: 'none',
+            [t.breakpoints.down("sm")]: {
+                display: "none",
             },
         },
     }));
@@ -70,71 +65,53 @@ function Landing() {
     const classes = useStyles();
 
     useEffect(() => {
-        document.documentElement.style.scrollBehavior = 'smooth';
+        document.documentElement.style.scrollBehavior = "smooth";
         return () => {
-            document.documentElement.style.scrollBehavior = '';
+            document.documentElement.style.scrollBehavior = "";
         };
     }, []);
 
     return (
-        <div className='landing'>
-            <div className='landing--container'>
+        <div className="landing">
+            <div className="landing--container">
                 <div
-                    className='landing--container-left'
+                    className="landing--container-left"
                     style={{ backgroundColor: theme.primary }}
                 >
-                    <div className='lcl--content'>
+                    <div className="lcl--content">
                         {socialsData.github && (
-
-                            <a
-                                href={socialsData.github}
-                                target='_blank'
-                                rel='noreferrer'
-                            >
+                            <a href={socialsData.github} target="_blank" rel="noreferrer">
                                 <FaGithub
-                                    className='landing--social'
+                                    className="landing--social"
                                     style={{ color: theme.secondary }}
-                                    aria-label='Github'
+                                    aria-label="Github"
                                 />
                             </a>
                         )}
                         {socialsData.linkedIn && (
-
-                            <a
-                                href={socialsData.linkedIn}
-                                target='_blank'
-                                rel='noreferrer'
-                            >
+                            <a href={socialsData.linkedIn} target="_blank" rel="noreferrer">
                                 <FaLinkedin
-                                    className='landing--social'
+                                    className="landing--social"
                                     style={{ color: theme.secondary }}
-                                    aria-label='LinkedIn'
+                                    aria-label="LinkedIn"
                                 />
                             </a>
                         )}
                         {socialsData.twitter && (
-                            <a
-                                href={socialsData.twitter}
-                                target='_blank'
-                                rel='noreferrer'
-                            >
+                            <a href={socialsData.twitter} target="_blank" rel="noreferrer">
                                 <FaTwitter
-                                    className='landing--social'
+                                    className="landing--social"
                                     style={{ color: theme.secondary }}
-                                    aria-label='Twitter'
+                                    aria-label="Twitter"
                                 />
                             </a>
                         )}
                         {socialsData.facebook && (
-                            <a
-                                href={socialsData.facebook}
-                                target='_blank'
-                                rel='noreferrer'
-                            >
+                            <a href={socialsData.facebook} target="_blank" rel="noreferrer">
                                 <FaFacebook
-                                    className='landing--social'
+                                    className="landing--social"
                                     style={{ color: theme.secondary }}
-                                    aria-label='Facebook'
+                                    aria-label="Facebook"
                                 />
                             </a>
                         )}
@@ -142,40 +119,35 @@ function Landing() {
                 </div>
                 <img
                     src={headerData.image}
-                    alt=''
-                    className='landing--img'
+                    alt="Ndiaga Ndiaye"
+                    className="landing--img"
                     style={{
-                        opacity: `${drawerOpen ? '0' : '1'}`,
+                        opacity: `${drawerOpen ? "0" : "1"}`,
                         borderColor: theme.secondary,
                     }}
                 />
                 <div
-                    className='landing--container-right'
+                    className="landing--container-right"
                     style={{ backgroundColor: theme.secondary }}
                 >
-                    <div
-                        className='lcr--content'
-                        style={{ color: theme.tertiary }}
-                    >
+                    <div className="lcr--content" style={{ color: theme.tertiary }}>
                         <h6>{headerData.title}</h6>
                         <h1>{headerData.name}</h1>
                         <p>{headerData.desciption}</p>
 
-                        <div className='lcr-buttonContainer'>
+                        <div className="lcr-buttonContainer">
                             {headerData.resumePdf && (
                                 <a
                                     href={headerData.resumePdf}
-                                    download='CV Ndiaga'
-                                    target='_blank'
-                                    rel='noreferrer'
+                                    download="CV Ndiaga"
+                                    target="_blank"
+                                    rel="noreferrer"
                                 >
-                                    <Button className={classes.resumeBtn}>
-                                        Voir mon CV
-                                    </Button>
+                                    <Button className={classes.resumeBtn}>Voir mon CV</Button>
                                 </a>
                             )}
                             <Link
-                                to='contacts'
+                                to="contacts"
                                 smooth={true}
                                 duration={500}
                                 className={classes.contactBtn}
--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,10 +1,10 @@
-import resume from '../assets/pdf/CV Ndiaga Ndiaye.pdf'
-import image from '../assets/png/Ndiaga-Ndiaye-Développeur-web.jpg'
+const resumePath = `${process.env.PUBLIC_URL}/assets/pdf/CV Ndiaga Ndiaye.pdf`;
+const imagePath = `${process.env.PUBLIC_URL}/assets/png/Ndiaga-Ndiaye-Développeur-web.jpg`;
 
 export const headerData = {
     name: 'Ndiaga Ndiaye',
     title: "Transporteur digital",
     desciption: "Je suis un couteau suisse digital, capable d'évoluer dans les domaines du développement web, des applications et du marketing digital. J'ai également des notions en infographie. Rigoureux et passionné par les défis, je travaille bien en équipe.",
-    image: image,
-    resumePdf: resume
+    image: imagePath,
+    resumePdf: resumePath
 }
